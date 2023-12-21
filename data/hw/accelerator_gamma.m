4721cbf4a4f8baa7715419d8f6d3437ebccaa205
LuigiAltamura <luigi.altamura@mail.polimi.it>
2023-12-21 18:50:05 +0100

    Added CMake support and script for the toolchain

---
+++ data/hw/accelerator_gamma.m
@@ -0,0 +1,5 @@
+num_pes: 168
+l1_size_cstr: 512
+l2_size_cstr: 108000
+noc_bw_cstr: 81920000
+offchip_bw_cstr: 512
--- find_best_mapping.py
+++ find_best_mapping.py
@@ -12,13 +12,13 @@ def main():
     min_sum_cycles = float('inf')  # Initialize with a large value
     min_sum_cycles_filename = ""
 
-    folder_name = f"{args.model}_gamma"
+    folder_name_gamma = f"{args.model}_results/{args.model}_gamma"
 
     total_runtime_cycles = 0 
 
     for numlayer in range(1, args.numlayers + 1):
         # Create dynamic filename
-        filename = os.path.join(folder_name, f"{args.model}_{numlayer}.csv")
+        filename = os.path.join(folder_name_gamma, f"{args.model}_{numlayer}.csv")
 
         # Check if the file exists
         if not os.path.exists(filename):
@@ -39,15 +39,33 @@ def main():
 
 
     avg_num_cycles = total_runtime_cycles/numlayer;
-    # Write the result to the output file
-    output_filename = os.path.join(folder_name, "output.txt")
+    
+    folder_name_custom = f"{args.model}_results/{args.model}_custom"
+    filename = os.path.join(folder_name_custom, f"{args.model}_custom.csv")
+    total_runtime_cycles_custom = 0
+    if not os.path.exists(filename):
+        print(f"Warning: File '{filename}' not found. Skipping.")
+
+    # Read CSV file into a pandas DataFrame
+    df = pd.read_csv(filename)
+
+    # Calculate the sum of the 'Runtime (Cycles)' column
+    sum_runtime_cycles = df[' Runtime (Cycles)'].sum()
+
+    total_runtime_cycles_custom += sum_runtime_cycles
+   
+   # Write the result to the output file
+    output_filename = os.path.join(f"{args.model}_results", "output.txt")
+
     with open(output_filename, "w") as output_file:
         output_file.write(f"File with minimum Runtime Cycles: {min_sum_cycles_filename}\n")
         output_file.write(f"Minimum Runtime Cycles: {min_sum_cycles} cycles\n")
         output_file.write(f"Average Runtime Cycles: {avg_num_cycles} cycles\n")
         output_file.write(f"{(1-min_sum_cycles/avg_num_cycles)*100}% smaller than average\n")
+        output_file.write(f"Tot Runtime Cycles: {total_runtime_cycles_custom}\n")
+        output_file.write(f"{(1-total_runtime_cycles_custom/min_sum_cycles)*100}% smaller than standard mapping\n")
 
-
+    
     print(f"Analysis complete. Result written to {output_filename}")
 
 if __name__ == "__main__":
